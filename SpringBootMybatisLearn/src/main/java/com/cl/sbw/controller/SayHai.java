b1b322ffdf78e9b143c82d1aced500844a47cd90
SkyList <skylist1988@163.com>
2018-02-05 16:53:27 +0800

    日志打印
    打印日志 打印sql

--- SpringBootMybatisLearn/src/main/java/com/cl/sbw/controller/SayHai.java
+++ SpringBootMybatisLearn/src/main/java/com/cl/sbw/controller/SayHai.java
@@ -1,11 +1,13 @@
 package com.cl.sbw.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cl.sbw.service.inter.TestService;
+
 
 @RestController
 public class SayHai {
@@ -16,6 +18,8 @@ public class SayHai {
 	@RequestMapping("hai")
 	public String hai() {
 		testService.selectTest();
+		Logger log = LoggerFactory.getLogger(this.getClass());
+		log.info("asdffd");
 		return "hai, how are you!";
 	}
 }
